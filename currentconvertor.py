0f271f65d4702c525e7e4793e70fc1ea1ff57124
edwincaceres24 <edwin.caceres@pucp.pe>
2020-11-28 16:37:43 -0500

    Comentario en archivo currency en rama M

--- currentconvertor.py
+++ currentconvertor.py
@@ -5,3 +5,5 @@ tc= 3.65
 print('Hola, ¿cuántos soles vas a cambiar?')
 moneda=float(input())
 print('De acuerdo, recibirás '+ str(moneda/tc)+ ' dólares')
+
+#Este comentario lo hice en la rama Master
