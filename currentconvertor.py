214c6bb03c8322fa8cb3077386517eb5704b8511
edwincaceres24 <edwin.caceres@pucp.pe>
2020-11-28 17:30:18 -0500

    Primer pase a Github

--- currentconvertor.py
+++ currentconvertor.py
@@ -6,8 +6,5 @@ print('Hola, ¿cuántos soles vas a cambiar?')
 moneda=float(input())
 print('De acuerdo, recibirás '+ str(moneda/tc)+ ' dólares')
 
-<<<<<<< HEAD
 #Este comentario lo hice en la rama Master
-=======
 #print Estos cambios los hice en la rama cambios
->>>>>>> cambios
