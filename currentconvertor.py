8e80865e9b3c494f0723a21ed9c8564b38ed681c
edwincaceres24 <edwin.caceres@pucp.pe>
2020-11-28 21:51:02 -0500

    Esto debe pasar todos los archivos

--- currentconvertor.py
+++ currentconvertor.py
@@ -9,3 +9,4 @@ print('De acuerdo, recibirás '+ str(moneda/tc)+ ' dólares')
 #Este comentario lo hice en la rama Master
 #print Estos cambios los hice en la rama cambios
 #Este comentario se subirá usando un protocolo de seguridad SSH
+#Este comentario debe hacerse con el archivo
