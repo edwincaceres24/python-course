e746b86052bca7e2a0ad69efecb08312ec1b4da3
edwincaceres24 <edwin.caceres@pucp.pe>
2020-12-03 12:38:48 -0500

    Se creó file os.py

---
+++ practic.py
@@ -0,0 +1,23 @@
+# empiza de 1
+# debes poner parámetros
+
+
+
+"""Ejercicio For"""
+
+for i in range(1,11,1):
+    print(i)
+# Escribir con while
+
+a=1
+while a<11:
+    print(a)
+    a=a+1
+
+"""Functions"""
+def hello(name):
+    print("Hola mundo "+ name)
+
+hello('Edwin')
+hello('Emilia')
+hello('Nathalie')
---
+++ pythonbook.py
@@ -0,0 +1,6 @@
+name = "a"
+#' ' es verdadero ya que es una variable definida
+import random
+for i in range(5):
+        print(random.randint(1,10))
+
