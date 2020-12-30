5bb26261a9c89f304bbf190010406c8292f93455
edwincaceres24 <edwin.caceres@pucp.pe>
2020-12-29 19:16:17 -0500

    Corrección para otros archivos

--- order_files.py
+++ order_files.py
@@ -1,31 +1,39 @@
+#Import moduls
 import os
-import sys
 import shutil
-import time
-from random import randint
 
-download_rutes ="/Users/administrador/Downloads"
+#Define variables
+download_rutes ="/Users/administrador/Downloads/"
 ext_text= ['.docx','.txt','.doc','.pdf','.pptx']
 ext_img=['.png','.jpg','.jpeg','.gif',]
 ext_video=['.mov','.mp4']
 ext_sfk=['.sfk']
 
-def order (file,ext):
-    for i in ext_text:
+
+def main():
+    for file in os.listdir(download_rutes): #Lista todos los archivos y carpetas
+        file_name, ext = os.path.splitext(file) #Separa el root y los formatos
+        order(file, ext)
+
+def order (file, ext):
+    for i in ext_text: #Hace un loop cuando ext_text
         if ext == i:
             shutil.move(download_rutes + file, download_rutes + 'Text')
+            print('Se ha movido el archivo ' + str(file) + ' a la carpeta Text')
     for i in ext_img:
         if ext == i:
             shutil.move(download_rutes + file, download_rutes + 'Images')
+            print('Se ha movido el archivo ' + str(file) + ' a la carpeta Images')
     for i in ext_video:
         if ext == i:
             shutil.move(download_rutes + file, download_rutes + 'Videos')
-    if ext != '':
-        shutil.move(download_rutes + file, download_rutes + 'Other')
+            print('Se ha movido el archivo ' + str(file) + ' a la carpeta Videos')
 
-def main():
-    for file in os.listdir(download_rutes):
-        file_name, ext = os.path.splitext(file)
-        order(file,ext)
+                        #evalúa que la extensión no sea vacía y no esté en los campos de lista
+    if  ext !='' and ((ext in (ext_video + ext_img + ext_sfk + ext_text))==False):
+        shutil.move(download_rutes + file, download_rutes + 'Other')
+        print('Este archivo ' + file + ' no tiene ninguna de la extensiones mencionadas. Se movió a la carpeta "Other')
 
 main()
+
+
